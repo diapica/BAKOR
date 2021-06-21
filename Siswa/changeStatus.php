d20988514112e3465dd6e854ae7e5937c8663e1e
Sandro <sandroowen@yahoo.co.id>
2021-06-22 01:14:24 +0700

    Refactor index.php and Change status message

--- Siswa/changeStatus.php
+++ Siswa/changeStatus.php
@@ -3,7 +3,7 @@
 include "../connection.php";
 $username = $_GET['username'];
 
-$sql = "update tbuser set status_register = 'a' where username='$username'";
+$sql = "UPDATE tbuser SET status_register = 'a' WHERE username='$username'";
 $query = mysqli_query($conn,$sql);
 header('location:pendaftaran.php');
 ?>
